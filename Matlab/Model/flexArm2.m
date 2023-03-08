68d849fd5afe7f6c6ae7d3745c655ce83a221da5
AleFire <alefire1@gmail.com>
2023-03-08 21:49:33 +0100

    Reordered files and added State_space_model livescript with corrections to JC model (original file untouched)

--- Matlab/Model/flexArm2.m
+++ Matlab/Model/flexArm2.m
@@ -1,4 +1,4 @@
-function dx = flexArm2(x,Jeq, JL, Beq, BL, Kt, Kg, Ks, Km, ng, nm, Rm, V)
+function dx = flexArm2(x, Jeq, JL, Beq, BL, Kt, Kg, Ks, Km, ng, nm, Rm, V)
 
 x1 = x(1); %theta
 x2 = x(2); %thetadot
@@ -6,9 +6,9 @@ x3 = x(3); %alpha
 x4 = x(4); %alphadot
 
 %% 
-tao = ng*nm*Kt*(V-Km*x2/Kt)/Rm;
+tau = ng*nm*Kt*Kg*(V-Km*Kg*x2)/Rm;
 
 dx(1,1) = x2;
-dx(2,1) = (tao - Beq*x2 + BL*x4 + Ks*x3)/Jeq;
+dx(2,1) = (tau - Beq*x2 + BL*x4 + Ks*x3)/Jeq;
 dx(3,1) = x4;
-dx(4,1) = -tao/Jeq - ((Jeq+JL)/(JL*Jeq))*BL*x4 + Beq/Jeq*x2 - ((Jeq+JL)/(JL*Jeq))*Ks*x3;
+dx(4,1) = -tau/Jeq - ((Jeq+JL)/(JL*Jeq))*BL*x4 + Beq/Jeq*x2 - ((Jeq+JL)/(JL*Jeq))*Ks*x3;
