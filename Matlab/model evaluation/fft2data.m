d2923f18be3f360668d535fc53fd311679b47fdc
Alp <alprecep.dayan@mail.polimi.it>
2023-03-17 13:26:07 +0100

    data load mismatch removed

--- Matlab/model evaluation/fft2data.m
+++ Matlab/model evaluation/fft2data.m
@@ -1,4 +1,4 @@
-function [fs,Pxs] = fft2data(data)
+function [fs,Pxs] = fft2data(data,Uvec )
 %UNTITLED Summary of this function goes here
 %   Detailed explanation goes here
 data = data;
@@ -7,21 +7,12 @@ data = data;
 Ts_002 = 0.002;
 downsample_factor   =   1;
 Ts                  =   Ts_002*downsample_factor;
-Uvec                =   data(2,1:downsample_factor:end);
-Time_vec            =   data(1,1:downsample_factor:end);
-N                   =   length(Time_vec);                               % Total number of data points
 
 %% Use noise-corrupted values
-conv2angle = (2*pi)/4096;
-theta           =  -data(3,1:downsample_factor:end)*conv2angle;               % Measured sideslip angle
-alfa            =  data(4,1:downsample_factor:end)*conv2angle;                % Measured yaw rate
-
-%calculate these using forward difference method
-thetadot = zeros(1,length(theta));
-alfadot = zeros(1,length(theta));
-thetadot(2:end) = 1*imgaussfilt([theta(2:end)- theta(1:end-1)]/Ts,0.99); 
-alfadot(2:end)  = 1*imgaussfilt([alfa(2:end)- alfa(1:end-1)]/Ts,0.99);
-
+theta           = -data(1,1:downsample_factor:end);               % Measured sideslip angle
+alfa            = data(2,1:downsample_factor:end);                % Measured yaw rate
+thetadot        = data(3,1:downsample_factor:end);
+alfadot         = data(4,1:downsample_factor:end);
 
 fs = [];
 Pxs = [];
--- Matlab/model evaluation/main.m
+++ Matlab/model evaluation/main.m
@@ -1,3 +1,7 @@
+close all
+clear all
+clc;
+%load datas
 load('./states_estimation.mat');
 states_estimation = statess_est;        % Optımızatıon based
 load('./states_pysics.mat');
@@ -5,16 +9,19 @@ states_pysics = statess_phy;            % Accordıng to datasheet
 load('./states_validation.mat');
 states_validation= states_val;          % Real Data
 
+load('./Uvec_val.mat');
+Uvec_val= Uvec_val;          % Real Data
 
-
-[fs Pxs_est] = fft2data(states_estimation);
-[fs Pxs_phy] = fft2data(states_pysics);
-[fs Pxs_val] = fft2data(states_validation);
+%fourier transformation for all states fs is frequencies, Px are the
+%magnitudes normalized 
+[fs Pxs_est] = fft2data(states_estimation,Uvec_val);
+[fs Pxs_phy] = fft2data(states_pysics,Uvec_val);
+[fs Pxs_val] = fft2data(states_validation,Uvec_val);
 
 evaluation_phy = compareffts(Pxs_val, Pxs_phy);
 evaluation_est = compareffts(Pxs_val, Pxs_est);
 
-display(["compatison pysics based error:"+evaluation_phy + " and optimization based error " + evaluation_est])
+display(["compatison pysics based error:"+ evaluation_phy + " and optimization based error " + evaluation_est])
 
 
 timerphy = comparetime(states_validation,states_pysics );
