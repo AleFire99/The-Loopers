9de02227388eae341082786e3c92013b2c840be6
AleFire99 <AleFire1@gmail.com>
2023-03-27 13:38:07 +0200

    Fixed frequency based control for base and tip in LQR control folder.

--- Matlab/Control_Schemes/2_Tip_LQR/Controller.m
+++ Matlab/Control_Schemes/2_Tip_LQR/Controller.m
@@ -20,33 +20,19 @@ DC_gain_2 = dcgain(TF);
 %The actual set time differs due to DC gain in the system
 
 C_desired = (s-poles(3))*(s-poles(4))/((s+24)*(s+25));      %Compensator scheme
+DC_C_des = dcgain(C_desired); 
 
-%gain = dcgain(s*L(1));
-
-t_set = 0.0065;         %if smaller weird complex numbers appears in C_PI
+t_set = 1.75;       %Good trade-off between speed and robustness
 tau = t_set/5;
-BW = 1/tau;
+BW = 1/(tau*DC_C_des*DC_gain_1(1));
 
-C_PI = C_desired*BW;
+C_comp = C_desired*BW;
 
-%PI scheme needs pre compensation!
+%PI not feasible, unstable poles, no phase margin!
 %C_PI = BW*(s-poles(3))*(s-poles(4))/s;
 
-% OL = minreal(C_PI*TF);
-% OL_1 = OL(1);
-% OL_2 = OL(2);
-% CL_1 = OL_1/(1+OL_1);
-% CL_2 = OL_2/(1+OL_2);
-
-%num_C_PI = cell2mat(C_desired.num);
-
-
-%TODO PID unstable! Non phase-minimum zeros!
-% K_D = num_C_PI(1);
-% K_P = num_C_PI(2);
-% K_I = num_C_PI(3);
-% 
-% R = K_P + 1/s*K_I + s*K_D;
+L = C_comp*TF(1);
+bode(L);
 
 
 %%
