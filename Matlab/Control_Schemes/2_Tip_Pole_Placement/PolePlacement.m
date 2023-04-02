3d2993a49603a6d963a94f1b9ba8f960c3d6545e
Arkozzo <andrea.archetti98@gmail.com>
2023-04-03 01:01:13 +0200

    Merge

--- Matlab/Control_Schemes/2_Tip_Pole_Placement/PolePlacement.m
+++ Matlab/Control_Schemes/2_Tip_Pole_Placement/PolePlacement.m
@@ -7,14 +7,21 @@ f=3.846;
 wn = 2*pi*f;
 zeta= 0.7;
 
+p_ol = pole(sysestc);
+
+A = sysestc.A;
+B = sysestc.B;
+C = sysestc.C;
+D = sysestc.D;
+
 %% Control with Pole Placement in Continous Time
 
 new_pole = [-wn*zeta ; -21; -23; -25];          %Definition of the new poles
 
 %Pole placement implementation
 
-k = place(sysestc.A, sysestc.B, new_pole);      
-syspp = ss(sysestc.A-sysestc.B*k,sysestc.B,sysestc.C,sysestc.D);
+k = place(A, B, new_pole);      
+syspp = ss(A-B*k,B,C,D);
 p_cl = pole(syspp);
 Kdc = dcgain(syspp);
 Kr = 1/Kdc(1);
@@ -22,6 +29,22 @@ Kr = 1/Kdc(1);
 %Observer implementation  
 
 L = place(sysestc.A',sysestc.C', 10*new_pole)'; 
+
+%Pole placement with Integral action
+
+A_en = [A, zeros(4,1);
+       -C(1,:), 0];
+B_en = [B; 0];
+C_en = [C, zeros(2,1)];
+D_en = [0;0];
+
+sys_en = ss(A_en, B_en, C_en, D_en);
+
+poles_en = [new_pole; 1.1*min(new_pole)];
+K_en = place(A_en, B_en, poles_en);
+K_x = K_en(1:4);
+K_eta = K_en(5);
+sys_pp_int = ss(A_en-B_en*K_en, B_en, C_en, D_en);
 
 %% Control with Pole Placement in Discrete Time
 new_pole2 = [0.95 + 0.0i, 0.98 + 0.0i, 0.96 + 0.0i, 0.993 - 0.0i];
