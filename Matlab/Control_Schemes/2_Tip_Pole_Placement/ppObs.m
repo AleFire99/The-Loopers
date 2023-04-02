33753340e6c3d7ba43a74a90ad39da78f764362d
AleFire <alefire1@gmail.com>
2023-04-02 14:57:30 +0200

    Added integral control to pole placement.

--- Matlab/Control_Schemes/2_Tip_Pole_Placement/ppObs.m
+++ Matlab/Control_Schemes/2_Tip_Pole_Placement/ppObs.m
@@ -1,15 +1,42 @@
+clc; clear all; close all;
+
+sysest = load("sysest.mat").sysest;
+
 %% continous
 sysestc = d2c(sysest);
 zeta = 0.8;
 wn = 24.1651;
+p_ol = pole(sysestc);
 new_pole = [-wn*zeta ; -21; -23; -25];
-k = place(sysestc.A, sysestc.B, new_pole);
-syspp = ss(sysestc.A-sysestc.B*k,sysestc.B,sysestc.C,sysestc.D);
+
+A = sysestc.A;
+B = sysestc.B;
+C = sysestc.C;
+D = sysestc.D;
+
+k = place(A, B, new_pole);
+syspp = ss(A-B*k,B,C,D);
 p_cl = pole(syspp);
 step(syspp)
 Kdc = dcgain(syspp);
 Kr = 1/Kdc;
-L = place(sysestc.A',sysestc.C', 10*new_pole)';
+L = place(A',C', 10*new_pole)';
+
+
+%Integral action
+A_en = [A, zeros(4,1);
+       -C(1,:), 0];
+B_en = [B; 0];
+C_en = [C, zeros(2,1)];
+D_en = [0;0];
+sys_en = ss(A_en, B_en, C_en, D_en);
+
+poles_en = [new_pole; 1.1*min(new_pole)];
+K_en = place(A_en, B_en, poles_en);
+K_x = K_en(1:4);
+K_eta = K_en(5);
+sys_pp_int = ss(A_en-B_en*K_en, B_en, C_en, D_en);
+
 %% discrete
 new_pole2 = [0.95 + 0.0i, 0.98 + 0.0i, 0.96 + 0.0i, 0.993 - 0.0i];
 k2 = place(sysest.A, sysest.B, new_pole2);
