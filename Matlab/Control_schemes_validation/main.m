c8d0b4563e523db33f67028d35dc49180d006ad6
AleFire <alefire1@gmail.com>
2023-04-16 11:59:05 +0200

    Tested and fixed scheme validation and added dead time calculation.

--- Matlab/Control_schemes_validation/main.m
+++ Matlab/Control_schemes_validation/main.m
@@ -6,31 +6,37 @@ clc
 Data = load("Data.mat").Data;
 time = Data(1,:);
 reference = Data(2,:);
-base = Data(3,:);
-tip = Data(4,:);
-total = Data(5,:);
+theta = Data(3,:);
+alpha = Data(4,:);
+tip = Data(5,:);
 control_signal = Data(6,:);
 integral_control_signal = Data(7,:);
 
 %%
 %Finding overshoot
 steady_state = reference(end);
-base_overshoot = max(base-steady_state)
-tip_overshoot = max(tip-steady_state)
-total_overshoot = max(total-steady_state)
+base_overshoot = max(theta-steady_state)
+tip_overshoot = max(abs(alpha))
+total_overshoot = max(tip-steady_state)
 
 %%
 %Finding settling time
-start_of_slope = find(total>0,1);
-slope = (total(start_of_slope+2)-total(start_of_slope))/(time(start_of_slope+2)-time(start_of_slope));
-time_constant = steady_state/slope;
+start_of_slope = find(tip>0.01,1);
+slope = (tip(start_of_slope+30)-tip(start_of_slope+20))/(time(start_of_slope+30)-time(start_of_slope+20));
+time_constant = steady_state/slope
 settling_time = 5*time_constant
 
 %%
 %Finding rising time
-low_out = find(total>0.1*steady_state);
-high_out = find(total>0.9*steady_state);
+low_out = find(tip>0.1*steady_state,1);
+high_out = find(tip>0.9*steady_state,1);
 rising_time = time(high_out)-time(low_out)
+
+%%
+%Finding dead time
+start_of_output = find(abs(tip)>10e-5,1);
+start_of_ref = find(reference>0.1,1);
+dead_time = time(start_of_output)-time(start_of_ref)
 
 %%
 %Finding maximum control signal and total control signal
