6e4d6a601cd50a4c7fa2069e48808c73b4ec35de
Arkozzo <andrea.archetti98@gmail.com>
2023-04-18 18:05:10 +0200

    Model Validation

--- Matlab/Model_Evaluation/compareffts.m
+++ Matlab/Model_Evaluation/compareffts.m
@@ -1,7 +1,6 @@
 function [error] = compareffts(Px1,Px2)
 
     diff = abs(Px1(:,:)-Px2(:,:));
-
     %error = norm(diff);
     error = sum(sum(abs(diff)));
     %error = sum(sum(diff./Px1))/length(diff)/4;
--- Matlab/Model_Evaluation/comparetime.m
+++ Matlab/Model_Evaluation/comparetime.m
@@ -1,8 +1,6 @@
 function [error] = comparetime(state1,state2)
-%Detailed explanation goes here
 
     diff = abs(state1-state2);
-
     error = sum(sum(diff));
 
 end
