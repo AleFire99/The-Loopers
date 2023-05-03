19fd0717612881f2326060e9bd89ce0e2c47c067
Arkozzo <andrea.archetti98@gmail.com>
2023-05-03 22:23:06 +0200

    Kalman filter

--- Matlab/Control_Schemes/2b_Tip_SS/Implementations/KalmanFilter.m
+++ Matlab/Control_Schemes/2b_Tip_SS/Implementations/KalmanFilter.m
@@ -2,7 +2,9 @@ function [L_KF] = KalmanFilter(sysest_ct_tip)
 
 Q_KF = eye(4);     %initial values
 R_KF = eye(1);
-         
+ 
+sysest_ct_tip.C
+
 L_KF = lqr(sysest_ct_tip.A.', sysest_ct_tip.C.', Q_KF, R_KF).';
 
 end
