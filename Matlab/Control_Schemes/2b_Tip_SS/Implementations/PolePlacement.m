b09736dbeaa456f7168dfef197f12b9bf2898c5a
Arkozzo <andrea.archetti98@gmail.com>
2023-05-05 01:07:40 +0200

    update observers

--- Matlab/Control_Schemes/2b_Tip_SS/Implementations/PolePlacement.m
+++ Matlab/Control_Schemes/2b_Tip_SS/Implementations/PolePlacement.m
@@ -1,7 +1,7 @@
-function [sys_controlled_pp, Kpp,K_p] = PolePlacement(sysest_ct,new_pole)
+function [sys_controlled_pp, Kpp,K_p] = PolePlacement(sysest_ct_tip,new_pole)
 
-Kpp = place(sysest_ct.A, sysest_ct.B, new_pole);      
-sys_controlled_pp = ss(sysest_ct.A - sysest_ct.B * Kpp,sysest_ct.B,sysest_ct.C,sysest_ct.D);
+Kpp = place(sysest_ct_tip.A, sysest_ct_tip.B, new_pole);      
+sys_controlled_pp = ss(sysest_ct_tip.A - sysest_ct_tip.B * Kpp,sysest_ct_tip.B,sysest_ct_tip.C,sysest_ct_tip.D);
 K_dc = dcgain(sys_controlled_pp);
 K_p = 1/K_dc(1);
 
