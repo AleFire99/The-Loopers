12f5cb45b8955721a1f84f0618cfa50438d0175d
Arkozzo <andrea.archetti98@gmail.com>
2023-05-04 16:00:52 +0200

    update simulink 2b

--- Matlab/Control_Schemes/2b_Tip_SS/Longsim_Signal.m
+++ Matlab/Control_Schemes/2b_Tip_SS/Longsim_Signal.m
@@ -1,6 +1,6 @@
 %% Parameters
 % Signals are 3 Steps, 3 Ramps, 35 Sinusoids
-
+% to add/remove/modify any signal modify the element in one of the vectors
 tip_max_steps = [20 45 90];
 tip_max_ramps = [20 45 90];
 frequencies_rads = 0.1*[ 010 030 050 070 090 100 110 120 130 140 150 160 170 180 185 190 193 195 197 200 205 210 220 230 240 250 260 270 280 290 300 340 370 400 450 500 ];
