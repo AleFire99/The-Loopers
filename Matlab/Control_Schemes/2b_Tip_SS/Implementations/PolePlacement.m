a6039492325c9f77239d616c9fdc211398b6b475
Arkozzo <andrea.archetti98@gmail.com>
2023-05-05 09:56:43 +0200

    Update of pp with PI

--- Matlab/Control_Schemes/2b_Tip_SS/Implementations/PolePlacement.m
+++ Matlab/Control_Schemes/2b_Tip_SS/Implementations/PolePlacement.m
@@ -1,7 +1,7 @@
-function [sys_controlled_pp, Kpp,K_p] = PolePlacement(sysest_ct_tip,new_pole)
+function [sys_controlled_pp, K_pp,K_p] = PolePlacement(sysest_ct_tip,new_pole)
 
-Kpp = place(sysest_ct_tip.A, sysest_ct_tip.B, new_pole);      
-sys_controlled_pp = ss(sysest_ct_tip.A - sysest_ct_tip.B * Kpp,sysest_ct_tip.B,sysest_ct_tip.C,sysest_ct_tip.D);
+K_pp = place(sysest_ct_tip.A, sysest_ct_tip.B, new_pole);
+sys_controlled_pp = ss(sysest_ct_tip.A - sysest_ct_tip.B * K_pp,sysest_ct_tip.B,sysest_ct_tip.C,sysest_ct_tip.D);
 K_dc = dcgain(sys_controlled_pp);
 K_p = 1/K_dc(1);
 
