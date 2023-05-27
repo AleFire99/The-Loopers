2eb0369bfc5af80a2a60ecd557fd9fb923496029
Arkozzo <andrea.archetti98@gmail.com>
2023-05-27 11:56:07 +0200

    Research of values in LQR

--- Matlab/2 - Control/Control schemes design/2b_Tip_SS/Implementations/Fire_LQRFF.m
+++ Matlab/2 - Control/Control schemes design/2b_Tip_SS/Implementations/Fire_LQRFF.m
@@ -1,10 +1,11 @@
 function [K_lqrFF_x, K_lqrFF_eta] = Fire_LQRFF(sysest_ct, tau)
 
-Q_lqr = diag([1000 1 100 1 10]);     %initial values
+%Q_lqr = diag([1 0.1 10 0.1 100]);     %initial values
+Q_lqr = diag([10 1 1000 1 1]);
 R_lqr = 1;
 
 A_tilde = [sysest_ct.A,zeros(4,1);
-           -1 0 -1 0 0];
+            -1 0 -1 0 0];
 
 A_tilde = A_tilde + tau*eye(5);
          
--- Matlab/2 - Control/Control schemes design/2b_Tip_SS/main.m
+++ Matlab/2 - Control/Control schemes design/2b_Tip_SS/main.m
@@ -59,33 +59,33 @@ v_a_max = 15;
                      
 pp_poles = [-23 -25 -27 -29];                  %JC/Arco
 
-[sys_controlled_pp, K_pp,K_p] = JC_PolePlacement(sysest_ct,pp_poles);
+%[sys_controlled_pp, K_pp,K_p] = JC_PolePlacement(sysest_ct,pp_poles);
 
 % Addition of the PI in the outer loop instead of just the proportional action
-
-wc_req = 2;
-s = tf('s');
-PI_pp = wc_req * K_p * (s + 1) / s;
+ 
+% wc_req = 2;
+% s = tf('s');
+% PI_pp = wc_req * K_p * (s + 1) / s;
 
 %% Pole placement with Integral action
 
-pp_pole_en = [-23 -25 -27 -29 -31];                       %Definition of the new poles
+%pp_pole_en = [-23 -25 -27 -29 -31];                       %Definition of the new poles
 
-[sys_controlled_pp_enla,K_pp_en_x,K_pp_en_eta] = Fire_PolePlacement_en(sysest_ct,pp_pole_en);
+%[sys_controlled_pp_enla,K_pp_en_x,K_pp_en_eta] = Fire_PolePlacement_en(sysest_ct,pp_pole_en);
 
 %% Fire's LQR
 
-[K_lqr] = Fire_LQRegulator(sysest_ct);
+%[K_lqr] = Fire_LQRegulator(sysest_ct);
 
 %% Fire's LQR with integrator and FeedForward
 
-tau = 5;    %Rate of convergence, real part of CL eigs smaller than tau
+%tau = 3;    %Rate of convergence, real part of CL eigs smaller than tau
 
-[K_lqrFF_x, K_lqrFF_eta] = Fire_LQRFF(sysest_ct, tau);
+%[K_lqrFF_x, K_lqrFF_eta] = Fire_LQRFF(sysest_ct_tip, tau);
 
 %% Arco's LQR
 
-tau = 15;
+tau = 20;
 
 [K_lqr_x, K_lqr_eta] = Arco_LQRegulator(sysest_ct_tip, tau);
 
@@ -96,8 +96,8 @@ tau = 15;
 
 %% Observer implementation  
 
-%obs_poles = 10*pp_poles;            %Arco
-obs_poles = 2*pp_poles;            %Jc
+obs_poles = 10*pp_poles;            %Arco
+%obs_poles = 2*pp_poles;            %Jc
 
 L_obs = StateObserver(sysest_ct,obs_poles);
 
@@ -109,3 +109,53 @@ R_KF = eye(1)*10e-8;
 %Q_KF = eye(4);     %Alp
 %R_KF = eye(1)*10;
 
+%% Comparison 
+
+% Enlarged Estimated System
+
+% A_tilde = [ sysest_ct_tip.A, -sysest_ct_tip.B*K_lqr_eta , sysest_ct_tip.B*K_lqr_x;
+%             -[1 0 1 0], -[0]*K_lqr_eta , -[0]*K_lqr_x;
+%             L_obs*sysest_ct.C, -sysest_ct_tip.B*K_lqr_eta, sysest_ct_tip.A-sysest_ct_tip.B*K_lqr_x];
+% B_tilde = [ zeros(4,1);
+%             eye(1,1);
+%             zeros(4,1)];       
+% C_tilde = [ sysest_ct.C, zeros(2,1), zeros(2,4)];
+% D_tilde = zeros(2,1);
+
+A_ob = sysest_ct.A - L_obs*sysest_ct.C;
+
+A_tilde = [ sysest_ct.A, -sysest_ct.B*K_lqr_eta , -sysest_ct.B*K_lqr_x;
+            -[1 0 1 0], -[0]*K_lqr_eta , -[0]*K_lqr_x;
+            L_obs*sysest_ct.C, -sysest_ct.B*K_lqr_eta, A_ob-sysest_ct.B*K_lqr_x];
+B_tilde = [ zeros(4,1);
+            eye(1,1);
+            zeros(4,1)];       
+C_tilde = [ sysest_ct_tip.C, zeros(2,1), zeros(2,4)];
+D_tilde = zeros(2,1);
+
+sys_controlled = ss(A_tilde, B_tilde, C_tilde,D_tilde); 
+
+%Plotting Part
+
+figure;
+bode(sysest_ct_tip(1),sys_controlled(1),{10e-2,10e6});
+legend('Uncontrolled Estimated Open Loop','Enlarged Estimated Closed Loop');
+grid;
+
+figure;
+margin(sysest_ct_tip(1));
+
+figure;
+pzmap(sys_controlled(1),'g');
+grid; legend('Enlarged Estimated Closed Loop');
+
+figure;
+hold on
+nyqplot(sys_controlled(1),'g');
+plot(-(1/v_a_max)*ones(10,1), linspace(-1,1,10))
+hold off
+grid; legend('Enlarged Estimated Closed Loop');
+
+L_poles_sys_controlled = pole(sys_controlled(1))
+L_zeros_sys_controlled = zero(sys_controlled(1))
+ 
